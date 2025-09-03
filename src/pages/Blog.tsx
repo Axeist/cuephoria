f59a33d1f2c33c19dde759fe0cfbd8d24aeecded
Axeist <160474257+Axeist@users.noreply.github.com>
2025-09-04 03:08:28 +0530

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -57,7 +57,7 @@ const blogPosts = [
     date: '2025-06-20',
     readTime: '5 min read',
     category: 'Family & Gaming',
-    image: 'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+    image: 'https://i.postimg.cc/xjNqDWQC/generated-image-3.png',
     icon: Coffee,
     gradient: 'from-orange-500 via-red-500 to-pink-500'
   },
