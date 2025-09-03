c294bd470bba357a06904149910d277938d26a8b
Axeist <160474257+Axeist@users.noreply.github.com>
2025-09-04 03:03:23 +0530

    Update BlogPost2.tsx

--- src/pages/BlogPost2.tsx
+++ src/pages/BlogPost2.tsx
@@ -60,7 +60,7 @@ const BlogPost2 = () => {
             
             <div className="relative rounded-3xl overflow-hidden mb-8 group">
               <img
-                src="https://images.unsplash.com/photo-1511512578047-dfb367046420?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+                src="https://i.postimg.cc/zfTRr6KK/Ultimate-Student-Hangout.png"
                 alt="Students enjoying gaming at Cuephoria"
                 className="w-full h-64 md:h-96 object-cover group-hover:scale-105 transition-transform duration-700"
               />
