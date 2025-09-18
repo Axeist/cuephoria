9aee0818ca3204b6f62ef2311b2ebe7cd6efd2d5
Axeist <160474257+Axeist@users.noreply.github.com>
2025-09-18 23:24:17 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.tsx'
 import './index.css'
 import './App.css'
@@ -8,6 +9,7 @@ import './App.css'
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <Analytics />
+    <SpeedInsights />
     <App />
   </HelmetProvider>
 );
